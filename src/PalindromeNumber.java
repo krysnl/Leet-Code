776812de6c86bfd2309b76ed76ad24a5c90b7d22
koray <koray.emresenel@hotmail.com>
2021-02-19 01:33:19 +0300

    Palindrome Number challenge solved

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,18 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int x = 316413;
+        System.out.println(isPalindrome(x));
+    }
+    public static boolean isPalindrome(int x) {
+        String str = Integer.toString(x);
+        int j = str.length();
+        for (int i = 0 ; i< str.length(); i++,j--) {
+            if(j < i+1)
+                break;
+            if (str.charAt(i) == str.charAt(j - 1))
+                continue;
+            else return false;
+        }
+        return true;
+    }
+}
