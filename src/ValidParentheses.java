1abd99b29f3c4382c4b195e637e760ca3b1165e7
koray <koray.emresenel@hotmail.com>
2021-05-17 03:00:02 +0300

    Valid Parentheses challenge solved

---
+++ src/ValidParentheses.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        String strs = "}";
+        System.out.println(isValid(strs));
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i<s.length() ; i++){
+            switch (s.charAt(i)) {
+                case '(' :
+                    stack.push(1);
+                    break;
+                case ')':
+                    if(stack.isEmpty())
+                        return false;
+                    if(stack.peek() == 1)
+                        stack.pop();
+                    else
+                        return false;
+                    break;
+                case '[' :
+                    stack.push(2);
+                    break;
+                case ']':
+                    if(stack.isEmpty())
+                        return false;
+                    if(stack.peek() == 2)
+                    stack.pop();
+                    else
+                        return false;
+                    break;
+                case '{' :
+                    stack.push(3);
+                    break;
+                case '}':
+                    if(stack.isEmpty())
+                        return false;
+                    if(stack.peek() == 3)
+                        stack.pop();
+                    else
+                        return false;
+                    break;
+            }
+        }
+
+
+        if(stack.isEmpty())
+        return true;
+
+        return false;
+    }
+}
