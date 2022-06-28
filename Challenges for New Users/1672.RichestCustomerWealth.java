b3be284829a3d1272403dafbb93751eaa4275758
sparkry <57563704+KorayEmreSenel@users.noreply.github.com>
2022-06-28 15:09:40 +0300

    Create 1672.RichestCustomerWealth.java

---
+++ Challenges for New Users/1672.RichestCustomerWealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int count = 0;
+        for(int i = 0 ; i<accounts.length ; i++){
+            count = 0;
+            for(int j = 0; j< accounts[0].length;j++){
+                count += accounts[i][j];
+            }
+            max = max < count ? count :max;
+        }
+        return max;
+    }
+}
